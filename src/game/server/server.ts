d548fdaa151677048b969610552fc1293d229871
ocanty <git@ocanty.com>
2018-10-21 20:30:56 +0100

    Setup express

--- src/game/server/server.ts
+++ src/game/server/server.ts
@@ -2,16 +2,26 @@ import { IEntryPoint } from '../shared/entryPoint'
 import { log } from './log'
 import { NetServer } from './net/netServer'
 
+import { WebServer } from '../../webServer/webServer'
+
+import * as dotenv from 'dotenv'
+
 class Server implements IEntryPoint {
-  private net : NetServer
+  private netServer : NetServer
+  private webServer : WebServer
 
   public run(argv : string[]) : void {
-    log('[picto] Server started')
-    // use parseInt for now
-    this.net = new NetServer(parseInt(argv[1], 10))
+    const webserverPort : number = parseInt(process.env.PORT, 10) || 8080
+    const websocketPort : number = parseInt(process.env.WS_PORT, 10)  || 8081
+
+    log('[picto] starting gameserver')
+
+    this.webServer = new WebServer(webserverPort)
+    this.netServer = new NetServer(websocketPort)
   }
 }
 
-const gameServer = new Server()
+dotenv.config()
 
-gameServer.run(['-p', '8080'])
+const gameServer = new Server()
+gameServer.run(process.argv)
