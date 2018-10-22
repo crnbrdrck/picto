34bdfa057e829506d7985de9cd36bdc58cf69f1e
ocanty <git@ocanty.com>
2018-10-22 19:24:48 +0100

    Fixed indendation

--- src/game/server/server.ts
+++ src/game/server/server.ts
@@ -7,18 +7,18 @@ import { WebServer } from '../../webServer/webServer'
 import * as dotenv from 'dotenv'
 
 class Server implements IEntryPoint {
-    private netServer: NetServer
-    private webServer: WebServer
+  private netServer: NetServer
+  private webServer: WebServer
 
-    public run(argv: string[]): void {
-        const webserverPort: number = parseInt(process.env.PORT, 10) || 8080
-        const websocketPort : number = parseInt(process.env.WS_PORT, 10) || 8081
+  public run(argv: string[]): void {
+    const webserverPort: number = parseInt(process.env.PORT, 10) || 8080
+    const websocketPort : number = parseInt(process.env.WS_PORT, 10) || 8081
 
-        log('[picto] starting gameserver')
+    log('[picto] starting gameserver')
 
-        this.webServer = new WebServer(webserverPort)
-        this.netServer = new NetServer(websocketPort)
-    }
+    this.webServer = new WebServer(webserverPort)
+    this.netServer = new NetServer(websocketPort)
+  }
 }
 
 dotenv.config()
